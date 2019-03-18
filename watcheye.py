e8acc6cf5291bc30ee8bcaab91f4b13c06d2fad4
Yosyp Schwab <y.shvab+git@gmail.com>
2019-03-18 15:47:48 -0400

    rudimentary logging boilerplate added

--- watcheye.py
+++ watcheye.py
@@ -1,8 +1,10 @@
 """
 Main module of the server file
 """
-from flask import render_template
 import connexion
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import render_template
 
 app = connexion.App(__name__, specification_dir="./")
 
@@ -10,8 +12,13 @@ app.add_api("swagger.yml")
 
 @app.route("/")
 def home():
+    app.app.logger.warning('A warning occurred (%d apples)', 42)
+    app.app.logger.error('An error occurred')
+    app.app.logger.info('Info')
     return render_template("home.html")
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
+    app.app.logger.addHandler(handler)
     app.run(debug=True)
