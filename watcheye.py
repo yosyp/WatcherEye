24b29e24643fbdd047d68abfe01b39c99da1909b
yosyp <y.shvab+git@gmail.com>
2019-03-20 09:40:12 -0400

    passing variables to Flask templates is easy!

--- watcheye.py
+++ watcheye.py
@@ -15,7 +15,7 @@ def home():
     # app.app.logger.warning('%s A warning occurred (%d apples)', request.remote_addr, 42)
     # app.app.logger.error('An error occurred')
     app.app.logger.info('%s: home() index route served', request.remote_addr)
-    return render_template("home.html")
+    return render_template("home.html", os = 'Windows')
 
 if __name__ == '__main__':
     handler = RotatingFileHandler('logs/log.log', maxBytes=10000, backupCount=1)
