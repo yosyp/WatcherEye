6d6499749bc23581ac76823c00309399e48d960e
yosyp <y.shvab+git@gmail.com>
2019-03-28 10:29:03 -0400

    confusing documentation update

--- tests/unit/test_ping.py
+++ tests/unit/test_ping.py
@@ -1,14 +1,14 @@
 """
-This file contains unit tests for the ping() GEt function of the ping API endpoint.
+This file contains unit tests for the Ping class get() method of the ping API endpoint.
 
 """
 import watchereye
 
 def test_ping():
     """
-    GIVEN ping GET endpoint function
-    WHEN the ping() function is triggered
-    THEN check that is returns a JSON with 'pong' as first element.
+    GIVEN ping GET endpoint method
+    WHEN the ping() method is triggered
+    THEN check that JSON string with 'pong' as first element is returned.
     """
     ping = watchereye.Ping()
 
